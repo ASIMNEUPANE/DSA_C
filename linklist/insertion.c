35f8b5b790b46f6a65bc949d2c693608969593fd
ASIMNEUPANE <asimneupane11@gmail.com>
2024-05-25 13:27:03 +0545

    insertionIn-4cases

---
+++ linklist/insertion.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h> //for malloc used
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkListTraversal(struct Node *ptr)
+{
+
+    while (ptr != NULL)
+    {
+        printf("element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *insertAtFirst(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+
+struct Node *insertInBetween(struct Node *head, int data, int index)
+{
+
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    int i = 0;
+    while (i != index - 1)
+    {
+        p = p->next;
+        i++;
+    }
+
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node *p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    ptr->next = NULL;
+    p->next = ptr;
+
+    return head;
+}
+struct Node *insertAfterNode(struct Node *head, struct Node *prevNode, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+
+    return head;
+}
+int main()
+{
+
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *forth;
+
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    forth = (struct Node *)malloc(sizeof(struct Node));
+
+    // Link first and second nodes
+    head->data = 7;
+    head->next = second;
+
+    // Link second and third nodes
+    second->data = 5;
+    second->next = forth;
+
+    // Termionate t
+    third->data = 9;
+    third->next = NULL;
+
+    forth->data = 44;
+    forth->next = third;
+
+    // head = insertAtFirst(head, 8);
+    // insertInBetween(head, 88, 0);
+    // insertAtEnd(head, 78);
+    head = insertAfterNode(head, second, 222);
+
+    linkListTraversal(head);
+    return 0;
+}
